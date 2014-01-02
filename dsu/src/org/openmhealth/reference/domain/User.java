78a524c44957b8c98c89719920e3a7be4a7af6ea
jojenki <john@openmhealth.org>
2014-01-02 11:04:28 -0500

    Minor code cleanup.
    Removed some unused code and modified the spacing (e.g. removed unnecessary white space, translated tabs into spaces, etc.).

--- dsu/src/org/openmhealth/reference/domain/User.java
+++ dsu/src/org/openmhealth/reference/domain/User.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 Open mHealth
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,12 +44,12 @@ public class User implements OmhObject {
 	 * The version of this class for serialization purposes.
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * The number of rounds for BCrypt to use when generating a salt.
 	 */
 	private static final int BCRYPT_SALT_ROUNDS = 12;
-	
+
 	/**
 	 * The group ID for the Jackson filter. This must be unique to our class,
 	 * whatever the value is.
@@ -60,7 +60,7 @@ public class User implements OmhObject {
 	static {
 		OmhObjectMapper.register(User.class);
 	}
-	
+
 	/**
 	 * The JSON key for the user's user-name.
 	 */
@@ -125,29 +125,29 @@ public class User implements OmhObject {
 
 	/**
 	 * Creates a new user.
-	 * 
+	 *
 	 * @param username
 	 *        This user's user-name.
-	 * 
+	 *
 	 * @param password
 	 *        This user's password.
-	 * 
+	 *
 	 * @param email
 	 *        The user's email address.
-	 * 
+	 *
 	 * @param registrationKey
 	 *        The unique key generated when this account was registered. This
 	 *        may be null if this account was never required to be activated.
-	 * 
+	 *
 	 * @param dateRegistered
 	 *        The date and time that this account was registered. This may be
 	 *        null if this account was never required to be activated.
-	 * 
+	 *
 	 * @param dateActivated
 	 *        The date and time that this account was activated. This may be
 	 *        null if this account was never required to be activated or if the
 	 *        owner has not yet registered it.
-	 * 
+	 *
 	 * @throws OmhException
 	 *         The user-name was invalid.
 	 */
@@ -160,16 +160,16 @@ public class User implements OmhObject {
 		@JsonProperty(JSON_KEY_DATE_REGISTERED) final Long dateRegistered,
 		@JsonProperty(JSON_KEY_DATE_ACTIVATED) final Long dateActivated)
 		throws OmhException {
-		
+
 		// Validate and store the user-name.
 		this.username = validateUsername(username);
-		
+
 		// Validate and store the password.
 		this.password = validatePassword(password);
-		
+
 		// Validate and store the email address.
-		this.emailAddress = validateEmail(email);
-		
+		emailAddress = validateEmail(email);
+
 		// Verify that the registration key and date align.
 		if(registrationKey == null) {
 			if(dateRegistered != null) {
@@ -186,33 +186,33 @@ public class User implements OmhObject {
 						"The account has a registration key but does not " +
 							"have a registration date.");
 			}
-			
+
 			// Verify that if a registration key was given that it is not empty.
 			if(registrationKey.length() == 0) {
 				throw new OmhException("The registration key is empty.");
 			}
 		}
-		
+
 		// Verify that the account was not registered after it was activated.
 		if(
-			(dateRegistered != null) && 
-			(dateActivated != null) && 
-			dateRegistered > dateActivated) {
-			
+			(dateRegistered != null) &&
+			(dateActivated != null) &&
+			(dateRegistered > dateActivated)) {
+
 			throw
 				new OmhException(
 					"The account was activated before it was registered.");
 		}
-		
+
 		// Store the registration and activation information.
 		this.registrationKey = registrationKey;
 		this.dateRegistered = dateRegistered;
 		this.dateActivated = dateActivated;
 	}
-	
+
 	/**
 	 * Returns the user's user-name.
-	 * 
+	 *
 	 * @return The user's user-name.
 	 */
 	public String getUsername() {
@@ -221,113 +221,120 @@ public class User implements OmhObject {
 
 	/**
 	 * Returns the user's hashed password.
-	 * 
-	 * @return The user's hashed password. 
+	 *
+	 * @return The user's hashed password.
 	 */
 	@JsonIgnore
 	public String getPassword() {
 		return password;
 	}
-	
+
 	/**
 	 * Returns the user's email address.
-	 * 
+	 *
 	 * @return The user's email address.
 	 */
 	public InternetAddress getEmail() {
 		return emailAddress;
 	}
-	
+
 	/**
 	 * Returns the generated key when the user was registered.
-	 * 
+	 *
 	 * @return The generated key when the user was registered.
 	 */
 	public String getRegistratioKey() {
 		return registrationKey;
 	}
-	
+
 	/**
 	 * Returns whether or not this account has been activated.
-	 * 
+	 *
 	 * @return Whether or not this account has been activated.
 	 */
 	public boolean isActivated() {
 		return ((registrationKey == null) || (dateActivated != null));
 	}
-	
+
 	/**
 	 * Returns the date and time that the account was registered or null if it
 	 * was never required to be activated. If {@link #isActivated()} returns
 	 * true, then this account was never required to be activated.
-	 * 
+	 *
 	 * @return The date and time that this account was registered or null.
 	 */
 	public Long getDateRegistered() {
 		return dateRegistered;
 	}
-	
+
 	/**
 	 * Returns the date and time that the account was activated or null if it
 	 * was never activated. If {@link #isActivated()} returns true, then this
 	 * account was never required to activate; otherwise, the user has not yet
 	 * activated.
-	 * 
+	 *
 	 * @return The date and time that this account was required to be
 	 *         registered or null.
 	 */
 	public Long getDateActivated() {
 		return dateActivated;
 	}
-	
+
+	/**
+     * Activates the user's account by setting the date the user was activated.
+     *
+     * @throws OmhException
+     *         The account has already been activated or the account is not
+     *         eligible for activation.
+     */
 	public void activate() throws OmhException {
 		if(isActivated()) {
 			throw new OmhException("The account has already been activated.");
 		}
-		
+
 		if(registrationKey == null) {
 			throw
 				new OmhException(
-					"This account is not elegible for activation.");
+					"This account is not eligible for activation.");
 		}
-		
+
 		dateActivated = System.currentTimeMillis();
 	}
-	
+
 	/**
 	 * Checks if a given password matches this user's password.
-	 * 
+	 *
 	 * @param password
 	 *        The plain-text password to check.
-	 * 
+	 *
 	 * @return True if the passwords match; false, otherwise.
-	 * 
+	 *
 	 * @throws OmhException
 	 *         The password is null.
 	 */
 	public boolean checkPassword(final String password) throws OmhException {
 		// Validate the parameter.
 		String validatedPassword = validatePassword(password);
-		
+
 		// Use BCrypt to check the password.
 		return BCrypt.checkpw(validatedPassword, this.password);
 	}
-	
+
 	/**
 	 * Verifies that a user-name is valid.
-	 * 
+	 *
 	 * @param username
 	 *        The user-name to validate.
-	 * 
+	 *
 	 * @return The trimmed user-name.
-	 * 
+	 *
 	 * @throws OmhException
 	 *         The user-name was invalid.
 	 */
 	public static String validateUsername(
 		final String username)
 		throws OmhException {
-		
+
 		if(username == null) {
 			throw new OmhException("The username is null.");
 		}
@@ -335,68 +342,68 @@ public class User implements OmhObject {
 		if(trimmedUsername.length() == 0) {
 			throw new OmhException("The username is empty.");
 		}
-		
+
 		return trimmedUsername;
 	}
-	
+
 	/**
 	 * Verifies that a password is not null or empty. This can be used for both
 	 * un-hashed and hashed passwords. This is not designed to hash passwords;
 	 * for that, use {@link #hashPassword(String)}. This is not designed to
 	 * verify that a plain-text password is the same as this user's password;
 	 * for that, use {@link #checkPassword(String)}.
-	 * 
+	 *
 	 * @param password The password to verify.
-	 * 
+	 *
 	 * @return The password exactly as it was given.
-	 * 
+	 *
 	 * @throws OmhException The password is null or empty.
 	 */
 	public static String validatePassword(
 		final String password)
 		throws OmhException {
-		
+
 		if(password == null) {
 			throw new OmhException("The password is null.");
 		}
 		if(password.length() == 0) {
 			throw new OmhException("The password is empty.");
 		}
-		
+
 		return password;
 	}
-	
+
 	/**
 	 * Hashes a plain-text password.
-	 * 
+	 *
 	 * @param plaintextPassword
 	 *        The plain-text password to hash.
-	 * 
+	 *
 	 * @return The hashed password.
-	 * 
+	 *
 	 * @throws OmhException
 	 *         The given password was invalid.
 	 */
 	public static String hashPassword(
 		final String plaintextPassword)
 		throws OmhException {
-		
+
 		String validatedPassword = validatePassword(plaintextPassword);
-		
+
 		return
 			BCrypt
 				.hashpw(validatedPassword, BCrypt.gensalt(BCRYPT_SALT_ROUNDS));
 	}
-	
+
 	/**
 	 * Verifies that an email address is a valid email address, but it does not
 	 * guarantee that the location that it references exists.
-	 * 
+	 *
 	 * @param email
 	 *        The email address to be validated.
-	 * 
+	 *
 	 * @return The email address as an InternetAddress object.
-	 * 
+	 *
 	 * @throws OmhException
 	 *         The email address is invalid.
 	 */
@@ -411,7 +418,7 @@ public class User implements OmhObject {
 		if(trimmedEmail.length() == 0) {
 			throw new OmhException("The email address is empty.");
 		}
-		
+
 		// Create the InternetAddress object.
 		InternetAddress result;
 		try {
@@ -423,7 +430,7 @@ public class User implements OmhObject {
 					"The email address is not a valid email address.",
 					e);
 		}
-		
+
 		// Validate the address.
 		try {
 			result.validate();
@@ -434,38 +441,7 @@ public class User implements OmhObject {
 				"The email address is not a valid email address.",
 				e);
 		}
-		
+
 		return result;
 	}
-	
-//	public static class UserJacksonFieldFilter implements JacksonFieldFilter {
-//		public static final String FILTER_ID =
-//			"org.openmhealth.reference.domain.User";
-//		
-//		/**
-//		 * A private constructor to ensure that only the User class ever
-//		 * instantiates this.
-//		 */
-//		private UserJacksonFieldFilter() {}
-//
-//		/*
-//		 * (non-Javadoc)
-//		 * @see org.openmhealth.reference.util.OmhObjectMapper.JacksonFieldFilter#getFilterId()
-//		 */
-//		@Override
-//		public String getFilterId() {
-//			return FILTER_ID;
-//		}
-//
-//		/*
-//		 * (non-Javadoc)
-//		 * @see org.openmhealth.reference.util.OmhObjectMapper.JacksonFieldFilter#getFieldNames()
-//		 */
-//		@Override
-//		public Set<String> getFieldNames() {
-//			Set<String> fieldNames = new HashSet<String>();
-//			fieldNames.add(JSON_KEY_PASSWORD);
-//			return fieldNames;
-//		}
-//	}
 }
